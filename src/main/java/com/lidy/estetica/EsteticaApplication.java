1718c819d63ac449111393da76d8a72069db4263
yIorran <iorran.gv@hotmail.com>
2025-03-17 20:40:10 -0300

    teste env vars

--- src/main/java/com/lidy/estetica/EsteticaApplication.java
+++ src/main/java/com/lidy/estetica/EsteticaApplication.java
@@ -12,6 +12,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = {"com.lidy.estetica"})
 public class EsteticaApplication {
     public static void main(String[] args) {
+        System.out.println("DB_URL: " + System.getenv("DB_URL"));
+        System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(EsteticaApplication.class, args);
     }
 }
